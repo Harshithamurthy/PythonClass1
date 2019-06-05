562dc621a965f23662931a45b378cdb3508b89fc
Harshithamurthy <harshitha.murthy94@gmail.com>
2019-06-05 12:16:54 +0530

    DemoCalculator and DemoInvoke

---
+++ DemoCalculator.py
@@ -0,0 +1,29 @@
+# def Add(a,b):
+#     c = a + b
+#     print("The Sum is ", c)
+#     return a, b, c
+#
+# c = Add(20,10)
+# print(c)
+
+def Add(a, b):
+    c = a + b
+    print("The Sum is ", c)
+
+
+def Sub(a, b):
+    c = a - b
+    print("The Sub is ", c)
+
+
+def Mul(a, b):
+    c = a * b
+    print("The Mul is ", c)
+
+
+def Div(a, b):
+    c = a / b
+    print("The Div is ", c)
+
+
+
---
+++ DemoInvoke.py
@@ -0,0 +1,14 @@
+# import DemoCalculator
+#
+# DemoCalculator.Add(40, 5)
+# DemoCalculator.Sub(40, 5)
+# DemoCalculator.Mul(40, 5)
+# DemoCalculator.Div(40, 5)
+
+# from DemoCalculator import Add, Sub
+from DemoCalculator import *
+
+Add(40, 7)
+Sub(30, 9)
+Mul(5, 10)
+Div(40, 5)
